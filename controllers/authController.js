aad59e603ef16d09c119eb9599ca54970c15b5f6
tnem <tanmay.e.mahale@gmail.com>
2024-08-17 21:05:06 +0530

    heroku-setup-done

--- controllers/authController.js
+++ controllers/authController.js
@@ -11,17 +11,22 @@ const signToken = (id) =>
     expiresIn: process.env.JWT_EXPIRES_IN,
   });
 
-const createSendToken = (user, statusCode, res) => {
+// const createSendToken = (user, statusCode, res) => {
+const createSendToken = (user, statusCode, req, res) => {
   const token = signToken(user._id);
   const cookieOptions = {
     expires: new Date( //                              hour min   sec milli sec
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000,
     ),
-    secure: true,
     httpOnly: true,
+    secure: req.secure || req.headers('x-forwarded-proto') === 'https',
   };
 
-  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+  // if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+  // if (req.secure || req.headers('x-forwarded-proto') === 'https')
+  //   cookieOptions.secure = true;
+  // cookieOptions.secure =
+  //   req.secure || req.headers('x-forwarded-proto') === 'https';
 
   res.cookie('jwt', token, cookieOptions);
 
@@ -51,7 +56,7 @@ exports.signup = catchAsync(async (req, res, next) => {
   const url = `${req.protocol}://${req.get('host')}/me`;
   await new Email(newUser, url).sendWelcome();
 
-  createSendToken(newUser, 201, res);
+  createSendToken(newUser, 201, req, res);
   // const token = signToken(newUser._id);
 
   // res.status(201).json({
@@ -79,7 +84,7 @@ exports.login = catchAsync(async (req, res, next) => {
   }
 
   // 3) If everything ok, send token to client
-  createSendToken(user, 200, res);
+  createSendToken(user, 200, req, res);
   // const token = signToken(user._id);
 
   // res.status(200).json({
@@ -266,7 +271,7 @@ exports.resetPassword = catchAsync(async (req, res, next) => {
   // done in the userModel.js - userSchmea.pre('save', function (next)
 
   // 4) Log the user in, send JWT
-  createSendToken(user, 200, res);
+  createSendToken(user, 200, req, res);
   // const token = signToken(user._id);
 
   // res.status(200).json({
@@ -291,7 +296,7 @@ exports.updatePassword = catchAsync(async (req, res, next) => {
   // User.findByIdAndUpdate will NOT work as intended!
 
   // 4) Log user in, send JWT
-  createSendToken(user, 200, res);
+  createSendToken(user, 200, req, res);
 });
 
 // exports.updatePassword = catchAsync(async (req, res, next) => {
